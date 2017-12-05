e647f9e7491fc35bbc4433834eb817010a21812c
Ammy <al4604@nyu.edu>
2017-12-05 15:45:16 -0500

    register done! working on bettering tags

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from appdef import app, conn
 import tags, content_edit_delete, friends, group, post_tag
-import getfriends, post_tag
+import getfriends, post_tag, register
 from post_tag import makePost
 import reply_post
 
