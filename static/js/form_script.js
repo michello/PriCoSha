f873d9960618c0a94968e1494d493200a1c3608d
ml4963 <ml4963@nyu.edu>
2017-12-08 00:38:18 -0500

    form selection

--- static/js/form_script.js
+++ static/js/form_script.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
     $("input[type='radio']").click(function(){
       if (this.value == 1) {
          $("#groupPost").hide();
+         $("input[name='friend_group_name']").prop('required',false);
       } else {
         $("#groupPost").show();
+        $("input[name='friend_group_name']").prop('required',true);
       }
     });
 });
